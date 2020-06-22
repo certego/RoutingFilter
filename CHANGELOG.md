51016d042bec26a700950a9bcbbfd034cc0b6565
eugenioseveri <eugies93@gmail.com>
2020-06-22 11:59:20 +0200

    Updated changelog and metadata

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,9 @@
 ## 1.0.x
+
+### 1.1.0
+#### Changes
+* Changed match output to to list of dicts, to allow multiple rules with different tags to match
+* Now the method laod_from_dicts returns nothing; a new getter has been added to retrieve the currently loaded rules
+
 ### 1.0.0
 * First release
--- setup.py
+++ setup.py
@@ -1,8 +1,11 @@
 from setuptools import setup
 
+with open("README.md", "r") as readme_file:
+    long_description = readme_file.read()
+
 setup(
     name='routingfilter',
-    version='1.0',
+    version='1.1.0',
     packages=['routingfilter'],
     include_package_data=True,
     install_requires=["IPy"],
@@ -10,5 +13,14 @@ setup(
     license='GNU LGPLv3',
     author='Certego S.r.l.',
     author_email='support@certego.net',
-    description='Generic Business Logic Implementation for Routing objects as python dictionaries'
+    description='Generic Business Logic Implementation for Routing objects as python dictionaries',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    classifiers=[
+        'License :: OSI Approved :: GNU Lesser General Public License v3 (LGPLv3)',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+    ],
+    python_requires='>=3.6'
 )
