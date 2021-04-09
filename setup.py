0e686a4db97d52fe5b756f7712b78257ba2e6565
Federico Foschini <ffoschini@certego.net>
2021-04-09 17:24:28 +0200

    Release 1.1.6

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as readme_file:
 
 setup(
     name='routingfilter',
-    version='1.1.5',
+    version='1.1.6',
     packages=['routingfilter'],
     include_package_data=True,
     install_requires=["IPy"],
