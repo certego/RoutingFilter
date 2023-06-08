55dac80264a2f3dae397ccdaaeb27deb59ae51b0
lorygold <lorygold10@gmail.com>
2023-06-08 17:09:29 +0200

    Added tests for STARTSWITH filter

--- routingfilter/routing_benchmark.py
+++ routingfilter/routing_benchmark.py
@@ -29,12 +29,12 @@ class RoutingBenchMark():
         rule = load_test_data("benchmark_rule_equals")
         for i in range(MAX_RULE):
             rule["streams"]["rules"]["mountain_bike"].append(my_dict)
-        self.benchmark_event_1_equals = load_test_data("benchmark_event_1_equals")
+        self.benchmark_event_1 = load_test_data("benchmark_event_1")
         self.routing.load_from_dicts([rule])
         start_time = datetime.now()
         # Sending 100 messages to the routing
         for i in range(MAX_EVENT):
-            self.routing.match(self.benchmark_event_1_equals)
+            self.routing.match(self.benchmark_event_1)
         end_time = datetime.now()
         print(f"{self.test1_EQUALS_no_key_match.__name__}: {(end_time - start_time).total_seconds()}")
 
@@ -49,14 +49,14 @@ class RoutingBenchMark():
         rule = load_test_data("benchmark_rule_equals")
         for i in range(MAX_RULE):
             rule["streams"]["rules"]["mountain_bike"].append(my_dict)
-        self.benchmark_event_1_equals = load_test_data("benchmark_event_1_equals")
+        self.benchmark_event_1 = load_test_data("benchmark_event_1")
         # Adding the field 'wheel_model' but with a value different from 'Superlight'
-        self.benchmark_event_1_equals.update({"wheel_model": "test"})
+        self.benchmark_event_1.update({"wheel_model": "test"})
         self.routing.load_from_dicts([rule])
         start_time = datetime.now()
         # Sending 100 messages to the routing
         for i in range(MAX_EVENT):
-            self.routing.match(self.benchmark_event_1_equals)
+            self.routing.match(self.benchmark_event_1)
         end_time = datetime.now()
         print(f"{self.test2_EQUALS_key_exists.__name__}: {(end_time - start_time).total_seconds()}")
     
@@ -75,13 +75,13 @@ class RoutingBenchMark():
         # Create 100 EQUALS same rules with 100 values
         for i in range(MAX_RULE):
             rule["streams"]["rules"]["mountain_bike"].append(my_dict)
-        self.benchmark_event_1_equals = load_test_data("benchmark_event_1_equals")
-        self.benchmark_event_1_equals.update({"wheel_model": "test"})
+        self.benchmark_event_1 = load_test_data("benchmark_event_1")
+        self.benchmark_event_1.update({"wheel_model": "test"})
         self.routing.load_from_dicts([rule])
         start_time = datetime.now()
         # Sending 100 messages to the routing
         for i in range(MAX_EVENT):
-            self.routing.match(self.benchmark_event_1_equals)
+            self.routing.match(self.benchmark_event_1)
         end_time = datetime.now()
         print(f"{self.test3_EQUALS_list_values.__name__}: {(end_time - start_time).total_seconds()}")
 
@@ -100,16 +100,109 @@ class RoutingBenchMark():
             rule["streams"]["rules"]["mountain_bike"].append(my_dict)
         self.routing.load_from_dicts([rule])
         # Create a list of values in the message for "wheel_model"
-        self.benchmark_event_1_equals = load_test_data("benchmark_event_1_equals")
-        self.benchmark_event_1_equals.update({"wheel_model": []})
+        self.benchmark_event_1 = load_test_data("benchmark_event_1")
+        self.benchmark_event_1.update({"wheel_model": []})
         for i in range(MAX_LIST_VALUES):
-            self.benchmark_event_1_equals["wheel_model"].append("test-" + str(i))
+            self.benchmark_event_1["wheel_model"].append("test-" + str(i))
         start_time = datetime.now()
         # Sending 100 messages to the routing
         for i in range(MAX_EVENT):
-            self.routing.match(self.benchmark_event_1_equals)
+            self.routing.match(self.benchmark_event_1)
         end_time = datetime.now()
         print(f"{self.test4_EQUALS_list_values_message.__name__}: {(end_time - start_time).total_seconds()}")
+
+    def test1_STARTSWITH_no_key_match(self):
+        """Performance test, for the STARTSWITH routing filter type, with:
+            - 100 same rules (type: STARTSWITH)
+            - 100 messages with 50 fields different from 'wheel_model'
+        """
+        self.routing = Routing()
+        my_dict = load_test_data("benchmark_rule_startswith_dict")
+        # Create 100 EQUALS same rules 
+        rule = load_test_data("benchmark_rule_startswith")
+        for i in range(MAX_RULE):
+            rule["streams"]["rules"]["mountain_bike"].append(my_dict)
+        self.benchmark_event_1 = load_test_data("benchmark_event_1")
+        self.routing.load_from_dicts([rule])
+        start_time = datetime.now()
+        # Sending 100 messages to the routing
+        for i in range(MAX_EVENT):
+            self.routing.match(self.benchmark_event_1)
+        end_time = datetime.now()
+        print(f"{self.test1_STARTSWITH_no_key_match.__name__}: {(end_time - start_time).total_seconds()}")
+
+    def test2_STARTSWITH_key_exists(self):
+        """Performance test, for the STARTSWITH routing filter type, with:
+            - 100 same rules (type: STARTSWITH)
+            - 100 messages with 50 fields (one of them 'wheel_model' but not 'Superlight'
+        """
+        self.routing = Routing()
+        my_dict = load_test_data("benchmark_rule_startswith_dict")
+        # Create 100 EQUALS same rules
+        rule = load_test_data("benchmark_rule_startswith")
+        for i in range(MAX_RULE):
+            rule["streams"]["rules"]["mountain_bike"].append(my_dict)
+        self.benchmark_event_1 = load_test_data("benchmark_event_1")
+        # Adding the field 'wheel_model' but with a value different from 'Superlight'
+        self.benchmark_event_1.update({"wheel_model": "test"})
+        self.routing.load_from_dicts([rule])
+        start_time = datetime.now()
+        # Sending 100 messages to the routing
+        for i in range(MAX_EVENT):
+            self.routing.match(self.benchmark_event_1)
+        end_time = datetime.now()
+        print(f"{self.test2_STARTSWITH_key_exists.__name__}: {(end_time - start_time).total_seconds()}")
+
+    def test3_STARTSWITH_list_values(self):
+        """Performance test, for the STARTSWITH routing filter type, with:
+            - 100 same rules (type: STARTSWITH)
+            - 100 messages with 50 fields (one of them 'wheel_model' but not 'Superlight'
+            - 100 different values in the rules (no matches with the message field)
+        """
+        self.routing = Routing()
+        my_dict = load_test_data("benchmark_rule_startswith_dict")
+        # Create a list of 100 values in the "value" field of the rule
+        for i in range(MAX_LIST_VALUES):
+            my_dict["filters"][0]["value"].append("test-" + str(i))
+        rule = load_test_data("benchmark_rule_startswith")
+        # Create 100 EQUALS same rules with 100 values
+        for i in range(MAX_RULE):
+            rule["streams"]["rules"]["mountain_bike"].append(my_dict)
+        self.benchmark_event_1 = load_test_data("benchmark_event_1")
+        self.benchmark_event_1.update({"wheel_model": "test"})
+        self.routing.load_from_dicts([rule])
+        start_time = datetime.now()
+        # Sending 100 messages to the routing
+        for i in range(MAX_EVENT):
+            self.routing.match(self.benchmark_event_1)
+        end_time = datetime.now()
+        print(f"{self.test3_STARTSWITH_list_values.__name__}: {(end_time - start_time).total_seconds()}")
+
+    def test4_STARTSWITH_list_values_exists(self):
+        """Performance test, for the EQUALS routing filter type, with:
+            - 100 values in the "wheel_model" of the rule and of the message, but none of them triggers a match (no 'Superlight')
+        """
+        self.routing = Routing()
+        my_dict = load_test_data("benchmark_rule_startswith_dict")
+        # Create a list of 100 values in the "value" field of the rule
+        for i in range(MAX_LIST_VALUES):
+            my_dict["filters"][0]["value"].append("test-" + str(i))
+        rule = load_test_data("benchmark_rule_startswith")
+        # Create 100 EQUALS same rules with 100 values
+        for i in range(MAX_RULE):
+            rule["streams"]["rules"]["mountain_bike"].append(my_dict)
+        self.routing.load_from_dicts([rule])
+        # Create a list of values in the message for "wheel_model"
+        self.benchmark_event_1 = load_test_data("benchmark_event_1")
+        self.benchmark_event_1.update({"wheel_model": []})
+        for i in range(MAX_LIST_VALUES):
+            self.benchmark_event_1["wheel_model"].append("test-" + str(i))
+        start_time = datetime.now()
+        # Sending 100 messages to the routing
+        for i in range(MAX_EVENT):
+            self.routing.match(self.benchmark_event_1)
+        end_time = datetime.now()
+        print(f"{self.test4_STARTSWITH_list_values_exists.__name__}: {(end_time - start_time).total_seconds()}")
 
 def main():
     routing_benchmark = RoutingBenchMark()
@@ -117,6 +210,10 @@ def main():
     routing_benchmark.test2_EQUALS_key_exists()
     routing_benchmark.test3_EQUALS_list_values()
     routing_benchmark.test4_EQUALS_list_values_message()
+    routing_benchmark.test1_STARTSWITH_no_key_match()
+    routing_benchmark.test2_STARTSWITH_key_exists()
+    routing_benchmark.test3_STARTSWITH_list_values()
+    routing_benchmark.test4_STARTSWITH_list_values_exists()
 
 if __name__ == "__main__":
     main()
