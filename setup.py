3c507682446dc303c75af39827ece791a70e1ccd
eugenioseveri <e.severi@certego.net>
2026-07-20 12:33:54 +0200

    Updated linters, python version and fixed bug in CI

--- setup.py
+++ setup.py
@@ -22,5 +22,5 @@ setup(
         "Programming Language :: Python :: 3",
         "Topic :: Software Development :: Libraries :: Python Modules",
     ],
-    python_requires=">=3.8",
+    python_requires=">=3.10",
 )
